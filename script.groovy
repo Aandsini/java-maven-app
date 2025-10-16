c80c68c4e8307520d64e5f62673ab4496cdcfa3e
Aandsini <muhammadsayyidfarhan08@gmail.com>
2025-10-16 15:11:43 +0700

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,12 +6,12 @@ def buildJar() {
 def buildImage() {
     echo "Building the Docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-     sh docker build -t capeahhhhhhhhh/demo-app:jma-2.0 .
-    sh "echo \$PASS | docker login -u \$USER --password-stdin"
-    sh docker push capeahhhhhhhhh/demo-app:jma-2.0 
-
+        sh 'docker build -t capeahhhhhhhhhh/demo-app:jmp-2.0 .'
+        sh "echo \$PASS | docker login -u capeahhhhhhhhhh --password-stdin"
+        sh 'docker push capeahhhhhhhhhh/demo-app:jmp-2.0'
     }
 }
+
 
 def deployApp() {
     echo 'Deploying the application...'
